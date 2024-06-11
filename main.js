9c2aab54c7997bc4e0ffc9ec200b1ae61c2243fe
tai19062004 <nguyentutai1964@gmail.com>
2024-06-12 02:29:27 +0700

    code moi

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+$(document).ready(function () {
+  new Swiper('.best-seller', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    centeredSlides: true,
+    breakpoints: {
+      768: {
+        centeredSlides: false,
+      },
+    },
+  });
+  new Swiper('.top-sell', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  });
+  new Swiper('.mix-match', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-mix-match-pagination',
+    },
+  });
+  new Swiper('.testimonials', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.swiper-testimonials-button-next',
+      prevEl: '.swiper-testimonials-button-prev',
+    },
+    breakpoints: {
+      768: {
+        centeredSlides: false,
+      },
+    },
+  });
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+  $('.my-rating').starRating({
+    totalStars: 5,
+    starShape: 'rounded',
+    strokeColor: '#894A00',
+    strokeWidth: 10,
+    starSize: 25,
+  });
+});
